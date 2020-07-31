7ad0e4f69c74c8d390d0867649085175f342df04
okazakihayato <okzkhyt0419waseda@outlook.jp>
2020-07-31 19:59:22 +0900

    Initial Commit

---
+++ LifeManagementSystem.v2Tests/LifeManagementSystem_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  LifeManagementSystem_v2Tests.swift
+//  LifeManagementSystem.v2Tests
+//
+//  Created by 岡崎隼斗 on 2020/07/31.
+//  Copyright © 2020 岡崎隼斗. All rights reserved.
+//
+
+import XCTest
+@testable import LifeManagementSystem_v2
+
+class LifeManagementSystem_v2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
